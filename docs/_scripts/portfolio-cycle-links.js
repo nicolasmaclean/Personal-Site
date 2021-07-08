55a39387238661e0498b5b278910dc3edba332a3
nick <nicolas.maclean2326@gmail.com>
2021-07-08 12:29:36 -0500

    fixed bugs specific to chrome and arrows to cycle are smarter
    The arrows use what project you are currently looking at to decide how to cycle and they use scroll to, so there will be less history in the pages and going back will be better.

--- docs/_scripts/portfolio-cycle-links.js
+++ docs/_scripts/portfolio-cycle-links.js
@@ -1,40 +1,79 @@
 window.addEventListener('load', (event) =>
 {
-    var leftArrow = document.getElementsByClassName('arrow-left')[0];
-    var rightArrow = document.getElementsByClassName('arrow-right')[0];
+    var projects = document.getElementsByClassName('portfolio-focus')[0];
+    var projectAmt = projects.childElementCount;
+    var projectsX = [];
+
+    // stores offset foreach project
+    for (let x = 0; x < projects.childElementCount; x++)
+    {
+        var child = projects.children[x];
+        projectsX.push(child.offsetLeft);
+    }
     
     // clicks the next image link
     function cycle(amt)
     {
-        var curActiveLink = window.location.href.split('#');
-        var baseurl = curActiveLink[0];
-        var projects = document.getElementsByClassName('portfolio-carousel')[0].childElementCount;
-        var nextLinkInt = 0;
-        
-        // if a link from the carousel has been selected
-        // else image link has not been clicked yet. I assume the viewer is still looking at the project 0.
-        if (curActiveLink.length > 1)
+        // finds the current element being viewed
+        var closest = 0;
+        var curOffset = projects.scrollLeft;
+
+        for (let x = 1; x < projectsX.length; x++)
         {
-            curActiveLink = parseInt(curActiveLink[1].split('-')[2]);
-            nextLinkInt = curActiveLink;
+            if (Math.abs(curOffset - projectsX[x]) < Math.abs(curOffset - projectsX[closest]))
+            {
+                closest = x;
+            }
         }
-        
-        nextLinkInt += amt;
-        
-        // keeps nextLinkInt within the appropriate range
-        nextLinkInt = nextLinkInt % projects;
-        
-        if (nextLinkInt < 0)
+
+        // cycles target
+        closest += amt;
+
+        if (closest < 0)
         {
-            nextLinkInt += projects;
+            closest += projectAmt;
         }
-        
+        else if (closest <= projectAmt)
+        {
+            closest %= projectAmt;
+        }
+
+        console.log(closest, projectAmt);
+
         // makes the move
-        window.location = baseurl + "#portfolio-project-thumbnail-" + nextLinkInt;
-        window.location = baseurl + "#portfolio-project-" + nextLinkInt;
+        var nextLink = projects.children[closest];
+        nextLink.scrollIntoView(true);
+        
+        
+        // chrome will only scroll one element at a time, so this uses a timeout, so it won't scroll to the thumbnail till its on the right project
+        if (navigator.userAgent.indexOf("Chrome") != -1 )
+        {
+            var scrollTarget = nextLink.offsetLeft;
+            var curScrollSRC = nextLink.parentElement;
+            
+            var ScrollInterval = window.setInterval(() =>
+            {
+                if (curScrollSRC.scrollLeft === scrollTarget)
+                {
+                    nextLink = document.getElementById('portfolio-project-thumbnail-' + closest);
+                    nextLink.scrollIntoView(true);
+                    window.clearInterval(ScrollInterval);
+                }
+            }, 50);
+        }
+        
+        // firefox allows both to scroll they are done simutaneously
+        else
+        {
+            nextLink = document.getElementById('portfolio-project-thumbnail-' + closest);
+            nextLink.scrollIntoView(true);
+        }
     }
 
     // adds events
+    var leftArrow = document.getElementsByClassName('arrow-left')[0];
+    var rightArrow = document.getElementsByClassName('arrow-right')[0];
+
     leftArrow.addEventListener('click', (event) =>
     {
         cycle(-1);
